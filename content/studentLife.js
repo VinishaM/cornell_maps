c9564b8e7b337de595a518fb8dbee2e55689e431
VinishaM <vm297@cornell.edu>
2018-04-28 14:13:50 -0400

    code for pins

---
+++ content/studentLife.js
@@ -0,0 +1,17 @@
+var content = [
+    {
+        location: {lat: 42.445904, lng: -76.483332},
+        title: "Sage Hall",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Sage Hall</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Sage Hall was opened in 1875 as an all-female dorm for female students on campus, propelled by a gift from Henry Sage' +
+        'that pushed the university to provide education to both men and women. </p>'+
+        '<p> Ezra Cornell placed a letter in the building\'s cornerstone, in which he layed out his goals for Cornell University, saying that ' +
+        '"Coeducation of the sexes and entire freedom from sectarian or political preferences is the only proper and safe way for providing an education that shall meet the wants of the future..."</p>'+
+        '</div>'+
+        '</div>',
+    }, 
+    {},
+]
