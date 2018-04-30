5a5cc4bf38fa67ae37bbc013eadfea0b0aafc607
VinishaM <vm297@cornell.edu>
2018-04-30 12:35:15 -0400

    founding and growth sections

---
+++ content/founding.js
@@ -0,0 +1,143 @@
+var content = [
+    {
+        location: {lat: 42.451191, lng:  -76.481507},
+        title: "Forest Home",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Forest Home</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Ezra Cornell owned much of the farmland that eventually became the site of Cornell University. He called the area Forest Park, and lived in a farmhouse that was named Forest Home. The site was later sold to the local Delta Tau Delta chapter.</p>' +
+        '<a href="https://ezramagazine.cornell.edu/WINTER11/Essentials.html"> Learn more</a></p>'+
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.447204, lng: -76.48442},
+        title: "Sage Chapel",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Sage Chapel</h1>'+
+        '<div id="bodyContent">'+
+        '<p>The Henry Sage Chapel was the first religious building on campus, completed in 1875. Many of the stained glass windows inside honor students that died, including WWII soldiers and civil rights protestors. Ezra Cornell is also buried in the chapel.</p>' +
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.450278, lng: -76.485373},
+        title: "White Hall",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">White Hall</h1>'+
+        '<div id="bodyContent">'+
+        '<p>White Hall was constructed in 1866 and was named after Andrew Dickson White, co-founder and first president of Cornell University. It was one of the first two buildings on Cornell’s campus (the other being Morrill Hall) and was referred to as ‘North University’ until it was renamed to White Hall in 1883.</p>' +
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.448863, lng: -76.485375},
+        title: "Morril Hall",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Morril Hall</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Morrill Hall was constructed in 1868 and was one of the first two buildings on Cornell’s campus (the other being White Hall). It was referred to as ‘South University’ when it was first constructed. Morrill Hall was named after Justin Smith Morrill, the Vermont senator who drove the' +
+        '<a href="https://en.wikipedia.org/wiki/Morrill_Land-Grant_Acts" Morrill Land-Grant Colleges Act of 1862</a>,  a key act that helped with the founding of Cornell University.</p>' +
+        '<p> Currently it houses the departments of Romance Studies, Russian Literature and Linguistics. </p>' +
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.449458, lng:  -76.485309},
+        title: "McGraw Hall",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">McGraw Hall</h1>'+
+        '<div id="bodyContent">'+
+        '<p>The last of the original 3 buildings on campus, McGraw Hall was built by the gift of trustee John McGraw - the first building to be named after its benefactor. Like Morrill and White, McGraw was constructed using Cayuga blue stone quarried from the bottom of Libe Slope. It originally housed the university library, 9 bells donated by Jennie McGraw (the original ' +
+        '<a href="https://olinuris.library.cornell.edu/uris-history/chime"> Cornell Chimes</a>) and a large Museum of Natural History. The museum was overseen by anatomy Professor Burt Wilder, who went on to found the Wilder Brain collection. At its peak, the collection contained over 1,200 brains.</p>' +
+        '<p> The building now houses various Humanities offices. </p>' +
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.446750, lng:  -76.491892},
+        title: "Llenroc",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Llenroc</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Llenroc was created from 1865 to 1875 to be home of Ezra Cornell, but Cornell died in 1874 before the house was completed' +
+        'His wife and children lived in the house for 32 years before moving out.' +
+        'His daughters ended up selling the house to the Delta Phi fraternity in 1911.' +
+        '<p><a href="https://en.wikipedia.org/wiki/Llenroc"> Learn more</a></p>'+
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.448257, lng: -76.481926},
+        title: "Andrew Dickson White House",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Andrew Dickson White House</h1>'+
+        '<div id="bodyContent">'+
+        '<p>The house was commissioned in 1871 by Andrew Dickson White, co-founder and the first president' +
+        'of Cornell University. There are multiple carvings meant to inspire students to find a higher purpose' +
+        'and appreciate beauty. The house was enacted at a site where White could see the whole university from' + 
+        'the doorstep, and it was used by many university presidents after him.</p>' +
+        '<p>Today, the building houses offices for the Humanities, and the house’s carriage house has been ' +
+        'turned into the Big Red Barn, a campus cafe.' +
+        '<p><a href="https://en.wikipedia.org/wiki/Andrew_Dickson_White_House"> Learn more</a></p>'+
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.452550, lng: -76.491108},
+        title: "Ezra’s Tunnel",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Ezra\'s Tunnel</h1>'+
+        '<div id="bodyContent">'+
+        '<p>In 1830, then 23 year old Ezra Cornell used gunpowder to make the 200 foot, 12 feet high, 13 feet ' +
+        'wide tunnel over 6 months. The tunnel was worked on one side at a time -- once the ends met, the' +
+        'tunnel was only off by two inches. The tunnel once sent water through the tunnel and powered mills ' + 
+        'along Fall Creek.</p>' +
+        '<p>Ezra’s tunnel was recently closed off January 2018 after 188 years of being open. It was the site of multiple deaths and drownings throughout the years.' +
+        '<p><a href="http://cornellsun.com/2018/01/28/cornell-gates-off-ezras-tunnel-after-188-years-and-2-deaths-this-decade/"> Learn more</a></p>'+
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.446407, lng: -76.484264},
+        title: "Barnes Hall",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Barnes Hall</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Barnes Hall was created in 1887. Barnes Hall was the first building in the country created for a college Christian Association and was built to help eradicate Cornell’s image as corrupt and sacrilegious. It was also the university’s first student union before Willard Straight was constructed.</p>' +
+        '<p><a href="https://www.fs.cornell.edu/fs/facinfo/fs_facilinfo.cfm?facil_cd=2009"> Learn more</a></p>'+
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.450708, lng: -76.486193},
+        title: "Morse Hall",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Morse Hall</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Morse Hall housed the university’s department of chemistry, but the building burned down in a fire in 1912 and was never rebuilt. The building is named after Samuel Morse, the inventor of the telegraph, who led Ezra Cornell to acquire the fortune he used to establish Cornell University.</p>' +
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.447902, lng: -76.484352},
+        title: "Boardman Hall",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Boardman Hall</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Boardman Hall was opened in 1892 to be the home of the Law School at Cornell. It was named after judge Douglas Boardman, the first dean of the law school. It eventually was torn down to create Olin Library, and busts from the building have been incorporated into the library’s inner and outer walls. </p>' +
+        '</div>'+
+        '</div>',
+    },
+]
---
+++ content/growth.js
@@ -0,0 +1,76 @@
+var content = [
+    {
+        location: {lat: 42.447812, lng: -76.485306},
+        title: "Uris Library",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">Uris Library</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Uris Library was the university’s first library building. It was gifted to Cornell by Henry Sage in honor the Jennie McGraw, who wanted to use her own fortune to fund a university library but was thwarted by her husband Willard Fiske (the university’s first librarian) in the ' +
+        '<a href="http://rmc.library.cornell.edu/collector/willcase/index.html">Great Will Case</a></p>' +
+        '<p>The AD White reading room (now commonly called the Harry Potter room) in Uris Library was originally created to  holding Andrew Dickson White’s impressive personal collection of over 30,000 books, which he donated to the Cornell Libraries. His influence can be found throughout the room’s design, with crests from Oxford and Cambridge in the stained glass, and metal structures to ensure the collection survived a fire. </p>'+
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.447602, lng: -76.485011},
+        title: "McGraw Tower",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">McGraw Tower</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Otherwise known as the "Binglee Dinglee", the clocktower attached to the library acts as a symbolism for the university’s non-sectarian ideals.  While most institutions placed their timepieces on university chapels, Cornell’s clocktower is attached to a library, emphasizing an education from literature rather than religion.</p>' +
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.447454, lng: -76.464906},
+        title: " College of Veterinary Medicine",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading"> College of Veterinary Medicine</h1>'+
+        '<div id="bodyContent">'+
+        '<p>The College of Veterinary Medicine was the first state college established at Cornell. Subjects taught at the college were in strong demand by the state government of New York, which had a need for people educated in veterinary medicine, so it agreed to fund the college after intense lobbying from the the college’s first professor James Law. </p>' +
+        '<p>The vet school was originally part of the present-day Ives Hall before it moved to is current home on east campus.</p>' +
+        '<p><a href="https://www2.vet.cornell.edu/about-us/our-history"> Learn more</a></p>'+
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.449696, lng: -76.479051},
+        title: " College of Home Economics",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading"> College of Home Economics</h1>'+
+        '<div id="bodyContent">'+
+        '<p>Propelled by the work of Martha Van Rensselaer, the Department of Home Economics at Cornell initially set out to meet the need of educating the large population of women and farmers wives about emerging scientific knowledge relating to subjects like hygiene, clothing, sanitation, food, nutrition and disease.</p>' +
+        '<p>After the college was established, Martha Van Rensselaer, with the help of Flora Rose, spent nearly 25 years laying a foundation for a College of Home Economics, which was later officially recognized as a state college. Despite their influential work, they were never offered full professorship, only the title of "lecturer".</p>' +
+        '<p>Now renamed the College of Human Ecology, the school still boasts a largely female body, with interdisciplinary courses ranging from environmental design and fiber science to policy management.</p>'+
+        '<p><a href="https://www.human.cornell.edu/about/history"> Learn more</a></p>'+
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.449054, lng: -76.477998},
+        title: " College of Agriculture",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading"> College of Agriculture</h1>'+
+        '<div id="bodyContent">'+
+        '<p>The New York State College of Agriculture was officially signed into legislation in 1904, amid avid support from botanist Liberty Hyde Bailey and large number of New York State farmers.</p>' +
+        '</div>'+
+        '</div>',
+    },
+    {
+        location: {lat: 42.445520, lng: -76.482057},
+        title: "School of Hotel Administration",
+        info: 
+        '<div id="content">'+
+        '<h1 id="firstHeading" class="firstHeading">School of Hotel Administration</h1>'+
+        '<div id="bodyContent">'+
+        '<p>The School of Hotel Administration was first opened in 1922 by a request from prominent hotel owners and the American Hotel Association. It was the first to offer an undergraduate degree program in hospitality management, and later the first master’s degree program. </p>' +
+        '<p><a href="https://sha.cornell.edu/about/history.html"> Learn more</a></p>'+
+        '</div>'+
+        '</div>',
+    }
+]
